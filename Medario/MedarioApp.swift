26dcff7ad87acfee8e2bcd15f524fe6208a9dcfb
Chris Remacle <chrisremacle483@gmail.com>
2021-11-02 10:23:58 -0400

    Initial Commit

---
+++ Medario/MedarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedarioApp.swift
+//  Medario
+//
+//  Created by Chris Remacle on 2021-11-02.
+//
+
+import SwiftUI
+
+@main
+struct MedarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
