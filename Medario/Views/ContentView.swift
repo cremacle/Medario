d95487aa5ebd64c1afb8f60b3e37ad39fae59d78
nsxcode <sahadeonatalie@gmail.com>
2021-11-05 14:10:48 -0400

    Styled the home page (ContentView)

--- Medario/Views/ContentView.swift
+++ Medario/Views/ContentView.swift
@@ -19,40 +19,48 @@ struct ContentView: View {
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
                 VStack{
-                    Text("Medario").font(.title)
+                    Text("Medario").font(.system(size: 60)).padding(.bottom)
                     Image("MedarioLogo")
                         .resizable()
                         .scaledToFill()
-                        .edgesIgnoringSafeArea(.all).frame(width: 130, height: 130, alignment: .center)
+                        .edgesIgnoringSafeArea(.all).frame(width: 130, height: 130, alignment: .center).padding(15)
+                        .padding(.bottom)
                     
-//                    NavigationLink(destination: SignInView()){
-//                        Text("Register")
-//                    }.buttonStyle(MenuButtonStyle()).frame( alignment: .topLeading)
+                    //TODO: Change destination to SignUpView
+                    NavigationLink(destination: SignInView()){
+                        Text("Register")
+                    }.buttonStyle(MenuButtonStyle()).frame( alignment: .topLeading)
                     
                     NavigationLink(destination: LoginView()){
                         Text("Login")
-                    }.buttonStyle(MenuButtonStyle()).frame( alignment: .topLeading)
-                }
+                    }.buttonStyle(MenuButtonStyle())
+                        .padding(.top)
                 
             }
-            
         }
 
     }
 }
+    
+//Custom Colors
+struct CustomColor {
+    static let lightPurple = Color("LightPurple")
+    static let bluePurple = Color("BluePurple")
+    
+}
 
+//Custom Button Style
 struct MenuButtonStyle: ButtonStyle{
     //ButtonStyle has a standard method that needs to be defined for it which is caled makeBody
     public func makeBody(configuration: Configuration) -> some View {
         configuration.label
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                
+            .frame(width: 129, height: 32, alignment: .center)
+            .padding()
+            .font(.headline)
+            .background(RoundedRectangle(cornerRadius: 10).fill(CustomColor.lightPurple))
+//            .border(CustomColor.bluePurple, width: 5)
+            .buttonStyle(.bordered)
     }
 }
 
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
 }
