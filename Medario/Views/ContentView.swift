352735f25070a2a9101a7981784ab927ed0423a5
nsxcode <sahadeonatalie@gmail.com>
2021-11-05 09:20:27 -0400

    Added title, logo, button for login. Added background image. Styling in progress.

--- Medario/Views/ContentView.swift
+++ Medario/Views/ContentView.swift
@@ -10,10 +10,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()        
+        NavigationView{
+
+            ZStack{
+               
+                Image("Background")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                VStack{
+                    Text("Medario").font(.title)
+                    Image("MedarioLogo")
+                        .resizable()
+                        .scaledToFill()
+                        .edgesIgnoringSafeArea(.all).frame(width: 130, height: 130, alignment: .center)
+                    
+//                    NavigationLink(destination: SignInView()){
+//                        Text("Register")
+//                    }.buttonStyle(MenuButtonStyle()).frame( alignment: .topLeading)
+                    
+                    NavigationLink(destination: LoginView()){
+                        Text("Login")
+                    }.buttonStyle(MenuButtonStyle()).frame( alignment: .topLeading)
+                }
+                
+            }
+            
+        }
+
     }
 }
+
+struct MenuButtonStyle: ButtonStyle{
+    //ButtonStyle has a standard method that needs to be defined for it which is caled makeBody
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
